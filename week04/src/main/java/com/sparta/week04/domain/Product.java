e06b382339b7457d514cab249f6b89eff6725f12
seokyeong <purewater71@naver.com>
2022-10-04 00:21:35 +0900

    [Add] spring week4

--- week04/src/main/java/com/sparta/week04/domain/Product.java
+++ week04/src/main/java/com/sparta/week04/domain/Product.java
@@ -1,4 +1,54 @@
 package com.sparta.week04.domain;
 
-public class Product {
+import com.sparta.week04.dto.ItemDto;
+import com.sparta.week04.dto.ProductMypriceRequestDto;
+import com.sparta.week04.dto.ProductRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Product extends Timestamped {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private String link;
+
+    @Column(nullable = false)
+    private int lprice;
+
+    @Column(nullable = false)
+    private int myprice;
+
+    // 관심 상품 생성
+    public Product(ProductRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.image = requestDto.getImage();
+        this.link = requestDto.getLink();
+        this.lprice = requestDto.getLprice();
+        this.myprice = 0;
+    }
+
+    // 관심 가격 변경
+    public void update(ProductMypriceRequestDto requestDto) {
+        this.myprice = requestDto.getMyprice();
+    }
+
+    public void updateByItemDto(ItemDto itemDto) {
+        this.title = itemDto.getTitle();
+        this.image = itemDto.getImage();
+        this.link = itemDto.getLink();
+        this.lprice = itemDto.getLprice();
+    }
 }
--- week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
+++ week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -1,10 +1,19 @@
 package com.sparta.week04.utils;
 
+import com.sparta.week04.dto.ItemDto;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.http.*;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component // @RequiredArgsConstructor 와 함께 사용할 경우 스프링이 컴포넌트 자동 생성함
 public class NaverShopSearch {
-    public String search() {
+    public String search(String query) {
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Naver-Client-Id", "uJfcCyUXGrJ8yy22NwyU");
@@ -12,7 +21,8 @@ public class NaverShopSearch {
         String body = "";
 
         HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
-        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=adidas", HttpMethod.GET, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=" + query, HttpMethod.GET, requestEntity, String.class);
+
         HttpStatus httpStatus = responseEntity.getStatusCode();
         int status = httpStatus.value();
         String response = responseEntity.getBody();
@@ -22,8 +32,23 @@ public class NaverShopSearch {
         return response;
     }
 
-    public static void main(String[] args) {
+    public List<ItemDto> fromJSONtoItems(String result) throws JSONException {
+        JSONObject rjson = new JSONObject(result);
+        JSONArray items = rjson.getJSONArray("items");
+        List<ItemDto> ret = new ArrayList<>();
+
+        for (int i=0; i<items.length(); i++) {
+            JSONObject itemJson = (JSONObject) items.get(i);
+            //System.out.println(itemJson);
+            ItemDto itemDto = new ItemDto(itemJson);
+            ret.add(itemDto);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) throws JSONException {
         NaverShopSearch naverShopSearch = new NaverShopSearch();
-        naverShopSearch.search();
+        String ret = naverShopSearch.search("나이키");
+        naverShopSearch.fromJSONtoItems(ret);
     }
 }
