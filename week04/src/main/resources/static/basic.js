0e6ed11097bb5b264c35c89df16d287da2e57433
seokyeong <purewater71@naver.com>
2022-10-10 22:03:19 +0900

    [Add] 4주차 과제

--- week04/src/main/resources/static/basic.js
+++ week04/src/main/resources/static/basic.js
@@ -146,8 +146,7 @@ function addProductItem(product) {
     // link, image, title, lprice, myprice 변수 활용하기
     return `<div class="product-card" onclick="window.location.href='${product.link}'">
                 <div class="card-header">
-                    <img src="${product.image}"
-                         alt="">
+                    <img src="${product.image}" alt="">
                 </div>
                 <div class="card-body">
                     <div class="title">
@@ -164,16 +163,24 @@ function addProductItem(product) {
 }
 
 function setMyprice() {
-    /**
-     * 숙제! myprice 값 설정하기.
-     * 1. id가 myprice 인 input 태그에서 값을 가져온다.
-     * 2. 만약 값을 입력하지 않았으면 alert를 띄우고 중단한다.
-     * 3. PUT /api/product/${targetId} 에 data를 전달한다.
-     *    주의) contentType: "application/json",
-     *         data: JSON.stringify({myprice: myprice}),
-     *         빠뜨리지 말 것!
-     * 4. 모달을 종료한다. $('#container').removeClass('active');
-     * 5, 성공적으로 등록되었음을 알리는 alert를 띄운다.
-     * 6. 창을 새로고침한다. window.location.reload();
-     */
+     let myprice = $('#myprice').val();
+
+     if (myprice == '') {
+         alert("값을 입력하세요.");
+         return;
+     }
+
+     $.ajax({
+        type: 'PUT',
+        url: '/api/products/${targetId}',
+        contentType: "application/json",
+        data: JSON.stringify({myprice: myprice}),
+        success: function (response) {
+            $('#container').removeClass('active');
+            alert("성공적으로 값이 설정되었습니다.");
+            window.location.reload();
+        }
+     })
+
+
 }
